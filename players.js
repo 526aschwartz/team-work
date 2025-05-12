2f3e1c194b499a5a26f71e63bbc250e6fb54b585
526aschwartz <526aschwartz@frhsd.com>
2025-05-12 10:40:20 -0400

    first commit

---
+++ players.js
@@ -0,0 +1,37 @@
+const players = [
+    {
+        firstName: 'Alyson',
+        lastName: 'Schwartz',
+        position: 'def',
+        age: 1,
+        photo: 'https://picsum.photos/200/300'
+    },
+     {
+        firstName: 'Alyson',
+        lastName: 'Schwartz',
+        position: 'def',
+        age: 1,
+        photo: 'https://picsum.photos/200/300'
+    },
+     {
+       firstName: 'Alyson',
+        lastName: 'Schwartz',
+        position: 'def',
+        age: 1,
+        photo: 'https://picsum.photos/200/300'
+    },
+     {
+        firstName: 'Alyson',
+        lastName: 'Schwartz',
+        position: 'def',
+        age: 1,
+        photo: 'https://picsum.photos/200/300'
+    },
+     {
+        firstName: 'Alyson',
+        lastName: 'Schwartz',
+        position: 'def',
+        age: 1,
+        photo: 'https://picsum.photos/200/300'
+    }
+]
