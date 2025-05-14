5ea8d853cbd6979d2f043af38e3f609c100ccc59
526aschwartz <526aschwartz@frhsd.com>
2025-05-14 09:31:44 -0400

    roster in progress

--- players.js
+++ players.js
@@ -1,37 +1,209 @@
 const players = [
     {
-        firstName: 'Alyson',
-        lastName: 'Schwartz',
-        position: 'def',
-        age: 1,
-        photo: 'https://picsum.photos/200/300'
-    },
-     {
-        firstName: 'Alyson',
-        lastName: 'Schwartz',
-        position: 'def',
-        age: 1,
-        photo: 'https://picsum.photos/200/300'
-    },
-     {
-       firstName: 'Alyson',
-        lastName: 'Schwartz',
-        position: 'def',
-        age: 1,
-        photo: 'https://picsum.photos/200/300'
-    },
-     {
-        firstName: 'Alyson',
-        lastName: 'Schwartz',
-        position: 'def',
-        age: 1,
-        photo: 'https://picsum.photos/200/300'
-    },
-     {
-        firstName: 'Alyson',
-        lastName: 'Schwartz',
-        position: 'def',
-        age: 1,
-        photo: 'https://picsum.photos/200/300'
+        firstName: "Nicolas",
+        lastName: "Aube-Kubel",
+        age: 29,
+        position: "RW",
+        photo: 'imgs/abue-kubel.png',
+        hasOver100Goals: false
     }
+  {
+        firstName: "Artemi",
+        lastName: "Panarin",
+        age: 33,
+        position: "LW",
+        photo: 'imgs/pan.png',
+        hasOver100Goals: true
+    },
+    {
+        firstName: "Mika",
+        lastName: 'Zibanejad',
+        age: 31,
+        position: "C",
+        hasOver100Goals: true
+    },
+    {
+        firstName: "Adam Fox",
+        age: 26,
+        position: "D",
+        hasOver100Goals: false
+    },
+    {
+        firstName: "Vincent",
+        lastName: "Trocheck",
+        age: 31,
+        position: "C",
+        photo: 'imgs/Trocheck.png',
+        hasOver100Goals: true
+    },
+    {
+        firstName: "Alexis",
+        lastName: "Lafrenière",
+        age: 23,
+        position: "LW",
+        photo: 'imgs/Lafrenière.png',
+        hasOver100Goals: false
+    },
+    {
+        firstName: "William",
+        lastName: "Cuylle",
+        age: 22,
+        position: "LW",
+        photo: 'imgs/Cuylle.png',
+        hasOver100Goals: false
+    },
+    {
+        firstName: "J.T.",
+        lastName: " Miller",
+        age: 32,
+        position: "C",
+        photo: 'imgs/JT miller.png',
+        hasOver100Goals: true
+    },
+    {
+        firstName: "Chris",
+        lastName: "Kreider",
+        age: 34,
+        position: "LW",
+        photo: 'imgs/Kreider.png',
+        hasOver100Goals: true
+    },
+    {
+        firstName: "Reilly Smith",
+        age: 33,
+        position: "RW",
+        hasOver100Goals: true
+    },
+    {
+        firstName: "K'Andre Miller",
+        age: 25,
+        position: "D",
+        hasOver100Goals: false
+    },
+    {
+        firstName: "Braden Schneider",
+        age: 23,
+        position: "D",
+        hasOver100Goals: false
+    },
+    {
+        firstName: "Filip Chytil",
+        age: 25,
+        position: "C",
+        hasOver100Goals: false
+    },
+    {
+        firstName: "Sam",
+        lastName: "Carrick",
+        age: 32,
+        position: "C",
+        photo: 'imgs/Carrick.png',
+        hasOver100Goals: false
+    },
+    {
+        firstName: "Ryan Lindgren",
+        age: 26,
+        position: "D",
+        hasOver100Goals: false
+    },
+    {
+        firstName: "Jonny",
+        lastName: "Brodzinski",
+        age: 31,
+        position: "C",
+        photo: 'imgs/Brodzinski.png',
+        hasOver100Goals: false
+    },
+    {
+        firstName: "Urho Vaakanainen",
+        age: 26,
+        position: "D",
+        hasOver100Goals: false
+    },
+    {
+        firstName: "Kaapo Kakko",
+        age: 23,
+        position: "RW",
+        hasOver100Goals: false
+    },
+    {
+        firstName: "William Borgen",
+        age: 28,
+        position: "D",
+        hasOver100Goals: false
+    },
+    {
+        firstName: "Zachary Jones",
+        age: 24,
+        position: "D",
+        hasOver100Goals: false
+    },
+    {
+        firstName: "Brett",
+        lastName: "Berard",
+        age: 22,
+        position: "LW",
+        photo: 'imgs/beared.png',
+        hasOver100Goals: false
+    },
+    {
+        firstName: "Adam",
+        lastName: "Edstrom",
+        age: 24,
+        position: "C",
+        photo: 'imgs/Edstrom.png',
+        hasOver100Goals: false
+    },
+    {
+        firstName: "Matt",
+        lastName: "Rempe",
+        age: 22,
+        position: "C",
+        photo: 'imgs/Rempe.png',
+        hasOver100Goals: false
+    },
+    {
+        firstName: "Jimmy Vesey",
+        age: 31,
+        position: "LW",
+        hasOver100Goals: true
+    },
+    {
+        firstName: "Jacob Trouba",
+        age: 30,
+        position: "D",
+        hasOver100Goals: false
+    },
+     {
+        firstName: "Arthur",
+        lastName: "Kaliyev",
+        age: 23,
+        position: "RW",
+        photo: 'imgs/Kaliyev.png',
+        hasOver100Goals: false
+    },
+    {
+        firstName: "Brennan",
+        lastName: "Othman",
+        age: 22,
+        position: "LW",
+        photo: 'imgs/othman.png',
+        hasOver100Goals: false
+    },
+    {
+        firstName: "Juuso",
+        lastName: "Parssinen",
+        age: 24,
+        position: "C",
+        photo: 'imgs/parrssinen.png',
+        hasOver100Goals: false
+    },
+    {
+        firstName: "Gabe",
+        lastName: "Perreault",
+        age: 20,
+        position: "RW",
+        photo: 'imgs/Perreault.png',
+        hasOver100Goals: false
+    },
 ]
